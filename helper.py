8058b7001bf9a248c04d6757368f5acd5817b6ce
Larry Wen <s1834299@PC061961.local>
2024-07-28 20:55:06 +1000

    keep q&A history and change to gpt 3.5

--- helper.py
+++ helper.py
@@ -11,6 +11,12 @@ st.header("2805ICT/3815ICT/7805ICT Milestone One Helper")
 
 training_file = "train.md"
 
+if 'qa_history' not in st.session_state:
+    st.session_state['qa_history'] = []
+
+
+# Button to read and process the input
+
 with open(training_file,"r") as file:
     text = file.read()
     spliter = RecursiveCharacterTextSplitter(
@@ -24,14 +30,16 @@ with open(training_file,"r") as file:
     embeddings = OpenAIEmbeddings(openai_api_key=key)
     vector_store = FAISS.from_texts(chunks,embeddings)
 
-    question = st.text_input("Please input your question")
+    # question = st.text_input("Please input your question")
+    question = st.text_input("Please input your question:")
+
     if question:
         matches = vector_store.similarity_search(question)
         llm = ChatOpenAI(
             openai_api_key=key,
             temperature=0,
             max_tokens=1000,
-            model='gpt-4o'
+            model='gpt-3.5-turbo-0125'
         )
         chain = load_qa_chain(llm, chain_type="stuff")
         # answer = chain.run(input_documents=matches,question=question)
@@ -42,6 +50,11 @@ with open(training_file,"r") as file:
 
         # Use the invoke method with the input dictionary
         answer = chain.invoke(input=input_data)        
+        st.session_state.qa_history.insert(0,(question, answer["output_text"]))
 
-        st.write(answer["output_text"])
+        # st.write(answer["output_text"])
         
+for i, (question, answer) in enumerate(st.session_state['qa_history']):
+    st.write(f"**Q:** {question}")
+    st.write(f"**A:** {answer}")
+    st.write("------------")
